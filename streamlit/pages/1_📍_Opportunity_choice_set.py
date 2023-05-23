78611f5a152163a2d1c523c47b19c7afd6c4db5c
Matti Pönkänen <mattiponk@gmail.com>
2023-05-23 20:32:18 +0300

    updates

--- streamlit/pages/1_📍_Opportunity_choice_set.py
+++ streamlit/pages/1_📍_Opportunity_choice_set.py
@@ -12,25 +12,15 @@ st.set_page_config(page_title="Opportunity choice-set",
                    layout="wide", 
                    initial_sidebar_state="expanded")
 
-## _________________ TABS ________________
-tab1, tab2 = st.tabs(['Number of opportunities', 'Distribution of opportunities'])
+st.markdown("""
+            ### 📍**Opportunity choice-set**
 
-with tab1:
-    st.markdown("""
-                ### 📍**Opportunity choice-set**
-                
-                The code in the backend is reading the *Bike stations* layer and sotre it in a variable.
-                You can calculate frequencies or statistics of your data and call them 
-                to the frontend as an informative message.
-                
-                For example, frequencies or statistics like:
-
-                *description of opportunities here*
-                
-                """)
+            *description of opportunities here*
+            
+            """)
 
 # Read in merged shapefile
-merged_opportunities = gpd.read_file('streamlit/data/merged_shapefile.shp')
+merged_opportunities = gpd.read_file('streamlit/data/merged_opportunities.shp')
 
 # Reproject the data to Web Mercator
 merged_opportunities = merged_opportunities.to_crs('EPSG:4326')
@@ -38,20 +28,32 @@ merged_opportunities = merged_opportunities.to_crs('EPSG:4326')
 # Get unique values from the 'municipality' column
 municipalities = merged_opportunities['mncplty'].unique()
 
-# Create a selectbox for municipalities
+# Add an "All" option to the municipalities list so that data can be looked at nationally
+municipalities = np.insert(municipalities, 0, 'Finland')
+
+# Create a selectbox for different municipalities
 selected_municipality = st.selectbox('Select a municipality', municipalities)
 
-# Filter the data based on the selected municipality
-filtered_data = merged_opportunities[merged_opportunities['mncplty'] == selected_municipality]
+# Filter the data based on the selected municipality or All
+if selected_municipality == 'Finland':
+    filtered_data = merged_opportunities
+    zoom_level = 5
+    point_size = 120
+else:
+    filtered_data = merged_opportunities[merged_opportunities['mncplty'] == selected_municipality]
+    zoom_level = 10
+    point_size = 80
 
+#----- CREATING A CHART -----
 
-# Summarize the number of each opportunity type for the selected municipality
-opportunities = filtered_data.groupby('opprtnt').size().reset_index(name='count')
+# Summarize the number of each opportunity type for the selected municipality or all
+opportunities = filtered_data.groupby(['opprtnt', 'color']).size().reset_index(name='count')
 
+# Rename the opportunity column
 opportunities = opportunities.rename(columns={'opprtnt': 'Opportunity type'})
 
 # Create a bar chart
-fig = px.bar(opportunities, x='Opportunity type', y='count', color='Opportunity type', text='count')
+fig = px.bar(opportunities, x='Opportunity type', y='count', color='color', text='count')
 
 # Update the layout of the chart
 fig.update_layout(
@@ -65,192 +67,46 @@ fig.update_layout(
 # Update the traces of the chart
 fig.update_traces(textposition='outside')
 
+#----- CREATING A MAP ALONGSIDE CHART -----
+
+# Calculate the centroid of the selected municipality's geometry so that map gets to the location of the points
+centroid = filtered_data.geometry.unary_union.centroid
+
 # Set the map style
 map_style = 'mapbox://styles/mapbox/light-v10'
 
 # Set the initial viewport for the map
 view_state = pdk.ViewState(
- latitude=60.164241183313685,
- longitude=24.93720902049495,
- zoom=10,
+ latitude=centroid.y,
+ longitude=centroid.x,
+ zoom=zoom_level,
  pitch=0
 )
-
+# XXXXXX Need to figure out how to get separate colors for different opportunity points XXXXX
 # Create a layer for the selected municipality
 municipality_layer = pdk.Layer(
  'GeoJsonLayer',
  data=filtered_data,
- get_radius = 50,
- get_fill_color=[255, 0, 0],
- pickable=True
+ get_radius = point_size,
+ get_fill_color=[18, 84, 199, 140],
+ get_line_color=[0, 0, 0],
+ pickable=True,
 )
 
-# Create a deck.gl map with both layers
+# Create a deck.gl map
 map = pdk.Deck(
     map_style=map_style,
     initial_view_state=view_state,
-    layers=[municipality_layer]
+    layers=[municipality_layer],
+     tooltip={
+         'html': '<b>Name:</b> {name}',
+         'style': {'backgroundColor': 'steelblue', 'color': 'white'}
+         }
 )
 
 # Display the chart and map in Streamlit using columns
-col1, col2 = st.columns(2)
-col1.plotly_chart(fig)
-col2.pydeck_chart(map)
-    # ## _____________ FREQUENCIES AND STATISTICS __________________ 
+col1, col2 = st.columns([1, 1])
+col1.plotly_chart(fig, use_container_width=True)
+col2.pydeck_chart(map, use_container_width=True)
 
-    # # read data
-    # bikes = gpd.read_file('streamlit/data/bike-sharing-system-stations.gpkg')
 
-    # # create DataFrame
-    # bikes_df = bikes[['Name', 'x', 'y', 'Kapasiteet']].rename(columns={'x': 'longitude', 'y':'latitude'})
-    # bikes_df = pd.DataFrame(bikes_df)
-
-    # # frequencies and stats
-    # count = bikes_df.Name.nunique()
-
-    # min = bikes_df.Kapasiteet.min()
-    # max = bikes_df.Kapasiteet.max()
-
-    # avg = round(bikes_df.Kapasiteet.mean(), 2)
-    # med = round(bikes_df.Kapasiteet.median(), 2)
-    # std = round(bikes_df.Kapasiteet.std(), 2)
-    # diff = round(avg-med, 2)
-
-    # ## _____________ DISPLAY VALUES __________________ 
-
-    # st.success(f'You have read successfully {count} Locations of bike stations at Helsinki-Finland')
-
-    # col1, col2 =  st.columns(2)
-
-    # with col1:
-    #     st.info(f'The minimum bike capacity is {min} from station {bikes_df.loc[bikes_df.Kapasiteet==min].Name.unique()[0]}')
-
-    #     st.metric('Average Capacity', value=avg, delta=f'{std} std')
-        
-    # with col2:
-    #     st.error(f'The maximum bike capacity is {max} from station {bikes_df.loc[bikes_df.Kapasiteet==max].Name.unique()[0]}')
-
-    #     st.metric('Median Capacity', value=med, delta=f'{diff} diff', delta_color='inverse')
-
-    
-    # with st.expander('Functions used', expanded=False):
-    #     st.code("""
-    #                 st.markdown()
-                    
-    #                 ---------------------------------------------------
-
-    #                 column1, column2 = st.columns(2)
-                    
-    #                 with column1:
-    #                     st.info("The minimum bike capacity...")
-                        
-    #                 with column2:
-    #                     st.info("The maximum bike capacity...")
-                        
-    #                 ---------------------------------------------------
-                    
-    #                 st.metric('Label', value = statistic, delta = rate)
-    #             """)
-        
-## -------------------------------------------------------------------------------------------------
-
-# with tab2:
-    
-#     ## _____________ MAP __________________ 
-
-#     st.markdown("""              
-#                 ### ***Simple maps with PyDeckGl***
-                
-#                 If you have a dataset with ***longitude*** and ***latitude*** you can create
-#                 a simple map with DeckGl. This function is already implemented in Streamlit 
-#                 and supports a quick visualization. Of course, you can create more elaborated 
-#                 maps but you have to create your own object and display it.
-                
-#                 Check out the next interactive examples:
-                
-#                 """)
-
-#     # options
-#     layers = ['Simple Scatter Map', 'Elaborated Scatter Map']
-
-#     # add box
-#     layer_type = st.selectbox('Choose the kind of visualization you want to display', options=layers, index=0)
-
-#     # conditional for layers
-#     if layer_type==layers[0]:
-                
-#         # map
-#         st.map(bikes_df, zoom=10)
-
-#         with st.expander('Code:', expanded=False):
-#             st.code("""
-#                     import streamlit as st
-                    
-#                     st.map(bikes_df, zoom=10)""")
-            
-#     if layer_type==layers[1]:
-        
-#         #map
-#         st.pydeck_chart(pdk.Deck(
-#                             map_style='mapbox://styles/mapbox/light-v9',
-#                             tooltip={"text": "{Name}: {Kapasiteet} Bikes"},
-                            
-#                 initial_view_state=pdk.ViewState(
-#                                                 latitude=60.171358,
-#                                                 longitude=24.941349,
-#                                                 zoom=11,
-#                                                 pitch=45,
-#                                                 ),
-#                 layers=[
-#                         pdk.Layer('ScatterplotLayer',
-#                                 data=bikes_df,
-#                                 pickable=True,
-#                                 opacity=0.6,
-#                                 stroked=True,
-#                                 get_position='[longitude, latitude]',
-#                                 get_color=[18, 84, 199],
-#                                 get_line_color=[0, 0, 0],
-#                                 line_width_min_pixels=1.5,
-#                                 get_radius='Kapasiteet',
-#                                 radius_scale=8,
-#                                 radius_min_pixels=1,
-#                                 radius_max_pixels=100)
-#                         ],
-#                     ))
-        
-#         with st.expander('Code:', expanded=False):
-        
-#             st.code("""
-                    
-#                 import pydeck as pdk
-#                 import streamlit as st
-                
-#                 st.pydeck_chart(pdk.Deck(
-#                             map_style='mapbox://styles/mapbox/light-v9',
-#                             tooltip={"text": "{Name}: {Kapasiteet} Bikes"},
-                            
-#                 initial_view_state=pdk.ViewState(
-#                                                 latitude=60.171358,
-#                                                 longitude=24.941349,
-#                                                 zoom=11,
-#                                                 pitch=45,
-#                                                 ),
-#                 layers=[
-#                         pdk.Layer('ScatterplotLayer',
-#                                 data=bikes_df,
-#                                 pickable=True,
-#                                 opacity=0.6,
-#                                 stroked=True,
-#                                 get_position='[longitude, latitude]',
-#                                 get_color=[18, 84, 199],
-#                                 get_line_color=[0, 0, 0],
-#                                 line_width_min_pixels=1.5,
-#                                 get_radius='Kapasiteet',
-#                                 radius_scale=8,
-#                                 radius_min_pixels=1,
-#                                 radius_max_pixels=100)
-#                         ],
-#                     ))
-#                 """)
-            
-# ## -------------------------------------------------------------------------------------------------
---
+++ streamlit/pages/2_🖥️_Routing_engine.py
@@ -0,0 +1,44 @@
+import streamlit as st
+from PIL import Image
+
+## _____________ Routing engine __________________ 
+
+# page
+st.set_page_config(page_title="Routing engine", 
+                   layout="wide", 
+                   initial_sidebar_state="expanded")
+
+st.markdown("""
+ ### 🖥️ **Routing engine**
+
+ *Routing with r5r is incredibly fast. For a large region you can easily calculate multiple scenarios during a one hour meeting.  
+ As the typical workflow of such routing tasks is shortened it opens up new possibilities to do more in dept analysis of distribution effects and to analyze different scenarios.*
+ 
+ """)
+
+# set CSS style for lineheight
+st.markdown(
+    """
+    <style>
+        .element-container p {
+            line-height: 1.5;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+image = Image.open('streamlit/data/Kuva4.png')
+st.image(image, use_column_width=True)
+
+# Style image max width
+st.markdown(
+    """
+    <style>
+        .element-container img {
+            max-width: 1000px;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
