894b8b08fab2422bf0d7d0ed298759166377abef
Matti Pönkänen <mattiponk@gmail.com>
2023-08-09 20:09:32 +0300

    fixing bugs and adding features

--- streamlit/pages/1_📍_Opportunity_choice_set.py
+++ streamlit/pages/1_📍_Opportunity_choice_set.py
@@ -4,124 +4,134 @@ import numpy as np
 import plotly.express as px
 import folium
 from streamlit_folium import folium_static
-from functions.markdown_functions import responsive_to_window_width
 
-st.set_page_config(page_title="Opportunity choice-set", 
-                   layout="wide", 
-                   initial_sidebar_state="expanded")
+def set_page():
+    st.set_page_config(page_title="Opportunity choice-set", 
+                    layout="wide", 
+                    initial_sidebar_state="expanded")
 
-st.markdown("""
-            ### 📍**Opportunity choice-set**
+    st.markdown("""
+                ### 📍**Opportunity choice-set**
 
-            *description of opportunities here*
-            
-            """)
+                *description of opportunities here*
+                
+                """)
+def read_data():
+    data = gpd.read_file('streamlit/data/merged_opportunities.shp')
+    # Reproject the data to Web Mercator
+    data = data.to_crs('EPSG:4326')
+    return data
 
-merged_opportunities = gpd.read_file('streamlit/data/merged_opportunities.shp')
+def filter_and_create_charts(data):
+    opportunity_types = data['opprtnt'].unique()
+    selected_types = st.multiselect('Select opportunity types:', opportunity_types)
 
-# Reproject the data to Web Mercator
-merged_opportunities = merged_opportunities.to_crs('EPSG:4326')
+    if not selected_types:
+        return None
 
-opportunity_types = merged_opportunities['opprtnt'].unique()
-selected_types = st.multiselect('Select opportunity types:', opportunity_types)
-
-if not selected_types:
-    st.warning('Please select at least one opportunity type.')
-
-else:
-    # Get unique values from the 'municipality' column
-    municipalities = merged_opportunities['mncplty'].unique()
-
-    # Add an "All" option to the municipalities list so that data can be looked at nationally
-    municipalities = np.insert(municipalities, 0, 'Finland')
-
-    # Create a selectbox for different municipalities
-    selected_municipality = st.selectbox('Select a municipality', municipalities)
-
-    # Filter the data based on the selected municipality or All
-    if selected_municipality == 'Finland':
-        filtered_data = merged_opportunities
-        zoom_level = 5
-        point_size = 120
     else:
-        filtered_data = merged_opportunities[merged_opportunities['mncplty'] == selected_municipality]
-        zoom_level = 10
-        point_size = 80
+        # Get unique values from the 'municipality' column
+        municipalities = data['mncplty'].unique()
+        # Add an "All" option to the municipalities list so that data can be looked at nationally
+        municipalities = np.insert(municipalities, 0, 'Finland')
+        # Create a selectbox for different municipalities
+        selected_municipality = st.selectbox('Select a municipality', municipalities)
+        # Filter the data based on the selected municipality or All
+        if selected_municipality == 'Finland':
+            filtered_data = data
+            zoom_level = 5
+        else:
+            filtered_data = data[data['mncplty'] == selected_municipality]
+            zoom_level = 10
+        # Filter data by selected opportunity types
+        filtered_data = filtered_data[filtered_data['opprtnt'].isin(selected_types)]
+        m, fig = create_charts(selected_municipality, filtered_data,zoom_level,opportunity_types)
 
-    # Filter data by selected opportunity types
-    filtered_data = filtered_data[filtered_data['opprtnt'].isin(selected_types)]
+        return m, fig
 
-    #----- CREATING A CHART -----
 
+def create_charts(selected_municipality, filtered_data, zoom_level,opportunity_types):
     # Summarize the number of each opportunity type for the selected municipality or all
-    opportunities = filtered_data.groupby(['opprtnt', 'color']).size().reset_index(name='count')
-
+    opportunity_sums = filtered_data.groupby(['opprtnt', 'color']).size().reset_index(name='count')
     # Rename the opportunity column
-    opportunities = opportunities.rename(columns={'opprtnt': 'Opportunity type'})
+    opportunity_sums = opportunity_sums.rename(columns={'opprtnt': 'Opportunity type'})
 
     # Create a bar chart
     fig = px.bar(
-        opportunities,
+        opportunity_sums,
         x='Opportunity type',
         y='count',
         color='Opportunity type',
         text='count',
-        color_discrete_sequence=opportunities['color'].unique()
+        color_discrete_sequence=opportunity_sums['color'].unique()
     )
-
     # Update the layout of the chart
     fig.update_layout(
-     title=f'Number of opportunities in {selected_municipality}',
-     title_font_size=24,
-     xaxis_title=None,
-     yaxis_title=None,
-     showlegend=False
+    title=f'Number of opportunities in {selected_municipality}',
+    title_font_size=24,
+    xaxis_title=None,
+    yaxis_title=None,
+    showlegend=False
     )
-
     fig.update_traces(textposition='outside')
 
     #----- CREATING A MAP ALONGSIDE CHART -----
-
     # Calculate the centroid of the selected municipality's geometry so that map gets to the location of the points
     centroid = filtered_data.geometry.unary_union.centroid
-
-    # Create a new Folium map centered on the centroid of the selected municipality's geometry
     m = folium.Map(location=[centroid.y, centroid.x], zoom_start=zoom_level, tiles = "cartodbpositron")
-
-    # Define a function for adding a point layer to the map
-    def add_point_layer(gdf, color):
-        # Add CircleMarkers to the map
-        for _, row in gdf.iterrows():
-            folium.CircleMarker(
-                location=[row.geometry.y, row.geometry.x],
-                radius=5,
-                color='white',
-                weight=0.8,
-                fill=True,
-                fill_color=color,
-                fill_opacity=1
-            ).add_child(folium.Tooltip(row['name'])).add_to(m)
-
-
     # Add a point layer to the map for each opportunity type
     for opportunity_type in opportunity_types:
         # Filter the data to only include rows for this opportunity type
         data = filtered_data[filtered_data['opprtnt'] == opportunity_type]
-        
         # Check if the data DataFrame is empty
         if not data.empty:
             # Get the color for this opportunity type from the first row of data
             color = data.iloc[0]['color']
-            
-            # Add a point layer to the map for this opportunity type
-            add_point_layer(data, color)
+            add_point_layer(data, color, m)
+
+    return m, fig
+
+# Define a function for adding a point layer to the map
+# Add a point layer to the map for this opportunity type
+def add_point_layer(gdf, color, m):
+    # Add CircleMarkers to the map
+    for _, row in gdf.iterrows():
+        folium.CircleMarker(
+            location=[row.geometry.y, row.geometry.x],
+            radius=5,
+            color='white',
+            weight=0.8,
+            fill=True,
+            fill_color=color,
+            fill_opacity=1
+        ).add_child(folium.Tooltip(row['name'])).add_to(m)
+
+def responsive_to_window_width():
+    making_map_responsive = """
+    <style>
+    [title~="st.iframe"] { width: 100%}
+    </style>
+    """
+    st.markdown(making_map_responsive, unsafe_allow_html=True)
+
+def main():
+    set_page()
+    data = read_data()
+    result = filter_and_create_charts(data)
+    if result is not None:
+        m, fig = result
+        col1, col2 = st.columns([1, 1])
+        col1.plotly_chart(fig, use_container_width=True)
+
+        responsive_to_window_width()
+        # Display the map in Streamlit
+        with col2:
+            with st.spinner(text="Loading map..."):
+                folium_static(m)
+    else:
+        # Handle the case where filter_and_create_charts returns None
+        st.warning('Please select at least one opportunity type.')
 
 
-    col1, col2 = st.columns([1, 1])
-    col1.plotly_chart(fig, use_container_width=True)
-
-    responsive_to_window_width()
-    # Display the map in Streamlit
-    with col2:
-        folium_static(m)
-
+if __name__ == "__main__":
+    main()
--- streamlit/pages/3_Closest_educational_facilities.py
+++ streamlit/pages/3_Closest_educational_facilities.py
@@ -1,7 +1,11 @@
 import streamlit as st
 import pandas as pd
+import geopandas as gpd
 import plotly.express as px
 import numpy as np
+import folium
+from streamlit_folium import folium_static
+from functions.markdown_functions import responsive_to_window_width
 
 
 ## _____________ OPPORTUNITY CHOICE-SET __________________ 
@@ -12,10 +16,12 @@ def set_page():
     initial_sidebar_state="expanded")
 
     st.markdown("""
-    ### 🚏**Accessibility data**
+    ### 📚✏️**Closest educational facilities**
 
-    *Here lets add a set of different cumulative figures to closest facilities*
-    
+    With the app below you can compare access differences between different Finnish municipalities. The figure plots the cumulative share of 7-17 old population against the travel time it takes 
+    to reach nearest educational facility, either with cycling or public transport. From the dropdown menu, select the municipality you want to analyze. By hovering over the cumulative figure
+    you can see a popup appear that has more detailed information. Methodology is described in more detail below the graphs.
+    <br><br>
     """)
 
 def read_data():
@@ -25,24 +31,29 @@ def read_data():
     grid = pd.read_csv('streamlit/data/grid.csv')
     # Extract unique values for municipality column
     municipality = pt_data['nimi'].unique()
-        # Add an option for all municipalities
-    municipality = np.append(municipality, "All municipalities")
+    # Add an option for all municipalities
+    municipality = np.insert(municipality, 0, "All municipalities")
+    # Store municipality variable in st.session_state
+    st.session_state.municipality = municipality
 
     return pt_data, cycling_data, grid, municipality
 
 
-def filter_data(pt_data, cycling_data, grid, municipality):
+def filter_data(pt_data, cycling_data, grid):
     
-    # Create selectbox widget
+    if 'selected_municipality' not in st.session_state:
+        st.session_state.selected_municipality = "All municipalities"
+
     option = st.selectbox(
-        'Select a municipality', municipality, index=len(municipality)-1
+        'Select a municipality', st.session_state.municipality, key='selected_municipality'
     )
+
 
     # Filter data based on selected municipality
     if option != "All municipalities":
-        pt_data = pt_data[pt_data['nimi'] == option]
-        cycling_data = cycling_data[cycling_data['nimi'] == option]
-        grid = grid[grid['nimi'] == option]
+        pt_data = pt_data[pt_data['nimi'] == option].copy()
+        cycling_data = cycling_data[cycling_data['nimi'] == option].copy()
+        grid = grid[grid['nimi'] == option].copy()
 
     # Delete negative values from population fields
     grid.loc[grid['he_7_12'] < 0, 'he_7_12'] = 0
@@ -63,13 +74,11 @@ def filter_data(pt_data, cycling_data, grid, municipality):
     cumulative_share_pt = [sum(pt_data.loc[pt_data['trv__50'] <= x, ['he_7_12', 'h_13_15', 'h_16_17']].sum(axis=1)) /
                             grid[['he_7_12', 'he_13_15', 'he_16_17']].sum().sum() for x in range(0, max_travel_time + 1)]
     cumulative_share_cycling = [sum(cycling_data.loc[cycling_data['trv__50'] <= x, ['he_7_12', 'h_13_15', 'h_16_17']].sum(axis=1)) /
-                                 grid[['he_7_12', 'he_13_15', 'he_16_17']].sum().sum() for x in range(0, max_travel_time + 1)]
-    
+                                grid[['he_7_12', 'he_13_15', 'he_16_17']].sum().sum() for x in range(0, max_travel_time + 1)]
 
-    # Reshape data for plotting
     data_long = pd.DataFrame({
-        'Travel time': list(range(0, max_travel_time + 1)) * 2,
-        'Population share': cumulative_share_pt + cumulative_share_cycling,
+        'travel_time': list(range(0, max_travel_time + 1)) * 2,
+        'access': cumulative_share_pt + cumulative_share_cycling,
         'mode': ['Public Transport + 1 000 m walk'] * (max_travel_time + 1) + ['Cycling'] * (max_travel_time + 1),
         'kunta': [option] * (max_travel_time + 1) * 2
     })
@@ -80,20 +89,73 @@ def filter_data(pt_data, cycling_data, grid, municipality):
 
 
 
+
 def create_fig(data_long):
     # Plot cumulative share line graph
     nimi = data_long['kunta'].iloc[0]
-    fig = px.line(data_long, x='Travel time', y='Population share', color='mode')
+    fig = px.line(data_long, x='travel_time', y='access', color='mode', custom_data=['mode'])
     fig.update_layout(
         title=f'Accessibility of nearest educational facilities in {nimi}',
         xaxis_title='Travel time to nearest educational institution (min)',
-        yaxis_title='Cumulative share of 7-17-year-old population (%)',
-        legend_title='Mode'
+        yaxis_title='Cumulative share of <br>7-17-year-old population (%)',
+        legend_title='Mode',
+        title_font=dict(
+            size=22
+        ),
+        xaxis=dict(
+            title_font=dict(size=18),
+            tickfont=dict(size=14)
+        ),
+        yaxis=dict(
+            title_font=dict(size=18),
+            tickfont=dict(size=14)
+        )
     )
-    fig.update_yaxes(tickformat='%')
+    fig.update_traces(hovertemplate='Travel time to nearest educational institution <b>%{x} min</b><br>Share of population that can access nearest opportunity: <b>%{y}</b>')
+    fig.update_yaxes(tickformat='.0%')
     return fig
 
 
+def create_map(municipality, selected_municipality):
+    municipality_polygons = gpd.read_file('streamlit/data/kunnat2023.shp')
+    municipality_polygons = municipality_polygons.to_crs('EPSG:4326')
+
+    # Select municipalities where the field in 'nimi' is same in municipality and municipality polygons and insert it to filtered_polygons
+    if selected_municipality != "All municipalities":
+        filtered_polygons = municipality_polygons[municipality_polygons['nimi'] == selected_municipality]
+        zoom_level = 7
+    else:
+        filtered_polygons = municipality_polygons
+        zoom_level = 5
+    centroid = filtered_polygons.geometry.unary_union.centroid
+    m = folium.Map(location=[centroid.y, centroid.x], zoom_start=zoom_level, tiles="cartodbpositron")
+
+    # Add filtered polygons to map
+    folium.GeoJson(filtered_polygons).add_to(m)
+    responsive_to_window_width()
+    # Display map
+    folium_static(m)
+
+def add_description():
+    st.markdown("""
+    ### **Methodology**
+    The data on this page has been created by using [R5R](https://github.com/ipeaGIT/r5r) to generate a travel time matrix between the central coordinates of the
+    [Finnish population grid](https://www.stat.fi/tup/ruututietokanta/index_en.html) and coordinates of [Finnish educational institutions](https://www.stat.fi/org/avoindata/paikkatietoaineistot/oppilaitokset_en.html).
+    up to 60 minutes. Based on the created travel time matrix, minimum travel cost to closest facilities has been calculated by using the [accessibility package](https://ipeagit.github.io/accessibility/#accessibility).
+    
+    **For cycling the following parameters were used:**\n
+    Level of traffic stress (LTS) tolerated by cyclist: 2 *(more info on LTS-values: https://docs.conveyal.com/learn-more/traffic-stress)*\n
+    Cycling speed: 15 km/h\n
+    Cycling speed for network sections that exceed the set LTS-value: 3.6 km/h (walking speed)\n
+    
+    **For public transport the following parameters were used:**\n
+    Departure time window: 7-7:30 am    *(more info on departure time window: https://ipeagit.github.io/r5r/articles/time_window.html)*\n
+    Maximum number of transfers per public transport trip: 1\n
+    Maximum distance one can walk to access, egress or transfer on a public transport trip: 1 km (for each leg of the journey)
+    
+    
+    
+    """)
 
 def main():
     set_page()
@@ -102,8 +164,14 @@ def main():
     except NameError:
         pt_data, cycling_data, grid, municipality = read_data()
     
-    fig = filter_data(pt_data, cycling_data, grid, municipality)
-    st.plotly_chart(fig)
+    fig = filter_data(pt_data, cycling_data, grid)
+    col1, col2 = st.columns([2.3,1])
+    with col1:
+        st.plotly_chart(fig, use_container_width=True, responsive=True)
+    with col2:
+        create_map(municipality, st.session_state.selected_municipality)
+
+    add_description()
 
 if __name__ == "__main__":
     main()
